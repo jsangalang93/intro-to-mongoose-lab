dda9fccce2ed21baa1f424064f716ce0c3641cd0
jsangalang93 <jsangalang93@gmail.com>
2024-04-24 13:58:49 -0400

    init?

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const mongoose = require('mongoose')
+const Todo = require('./models/schema.js')
+
 const prompt = require('prompt-sync')();
 
 const username = prompt('What is your name? ');
